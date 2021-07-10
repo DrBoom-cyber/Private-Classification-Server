64e2442068036f47486dd156e224b24966af5928
Dallin Hutchison <junkformyblog@gmail.com>
2021-07-10 11:38:22 -0600

    all files we need

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask import jsonify
+from flask import request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
