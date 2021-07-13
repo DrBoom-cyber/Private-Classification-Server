e408a2717130fca64ac96ef225b5866814296c67
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2021-07-13 12:27:33 -0700

    Added .gitignore, updated data_manager methods

--- backend/data_manager.py
+++ backend/data_manager.py
@@ -1,4 +1,5 @@
 import os
+import ntpath
 
 #Recieves output configuration which is pairs of paths and resolution_keys
 class DataManager():
@@ -9,14 +10,30 @@ class DataManager():
         self.hash_length = hash_length
         self.data_map = self.generate_hashes()
 
-    def request_item(self):
-        pass
+        self.available_items = self.data_map.keys()
+        self.in_use = []
 
-    def resolve_item(self, item_hash, resolution_key):
-        pass
+    def request_item(self):
+        item_key = self.available_items.pop()
+        self.in_use.append(item_key)
+        return {
+            'image_url' : self.data_map[item_key]
+            'item_key' : item_key
+            'options' : self.output_config.keys()
+        }
+
+    def resolve_item(self, item_key, resolution_key):
+        if item_key in self.data_map.keys():
+            item = self.data_map[item_key]
+        else:
+            raise KeyError(f'Invalid item key, no data item exists with key {item_key}')
+        if resolution_key in self.output_config.keys():
+            os.rename(item, os.path.join(self.output_config[resolution_key], ntpath.basename(item)))
+        else:
+            raise KeyError(f'Invalid resolution key, no output item exists with key {resolution_key}')
 
     def generate_hashes(self):
         data_map = {}
-        for path in os.list_dir(self.data_path):
+        for path in os.listdir(self.data_path):
             data_map[os.urandom(self.hash_length)] = path
         return data_map
